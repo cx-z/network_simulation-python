c4a6678fec5605dd835a51b2e11a90229ac2eadd
cx-z <1612379420@qq.com>
2018-12-22 23:28:10 +0800

    Add files via upload

--- Node.py
+++ Node.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-import Packet
+import Packet, sys
 
 
 def check(pkt):
@@ -12,28 +12,30 @@ class Node(object):
     def __init__(self, seq):
         self.name = seq
         self.packets = []
-        self.src = self
-        self.dst = self
         self.nextHop = self
         self.flag = False
 
     # 从上一跳接收数据包
     def receive_packets(self, pkt, route_table):
-        self.packets.append(pkt)
+        temp_packet = Packet.Packet()
+        temp_packet.content = pkt.content
+        temp_packet.dst = pkt.dst
+        temp_packet.src = pkt.src
+        self.packets.append(temp_packet)
+        # if len(self.packets) > 5:
+        #     del self.packets[0]
         if self.nextHop != self:  # 如果不是目的节点，向下一跳发送数据包
             self.flag = True
-            self.send_packets(pkt, route_table)
+            self.send_packets(temp_packet, route_table)
 
     # 发送数据包到下一跳
     def send_packets(self, pkt, route_table):
-        self.src = pkt.src
-        self.dst = pkt.dst
         if pkt.src == self:
             self.flag = True
         if self.flag:
             self.nextHop = route_table[route_table.index(self) + 1]
-            self.nextHop.receive_packets(pkt, route_table)
             self.flag = False
+            self.nextHop.receive_packets(pkt, route_table)
 
 
 class CheckNode(Node):
@@ -44,28 +46,34 @@ class CheckNode(Node):
         self.check_state = False
 
     def receive_packets(self, pkt, route_table):
-        self.packets.append(pkt)  # 收到数据包
+        temp_packet = Packet.Packet()
+        temp_packet.content = pkt.content
+        temp_packet.dst = pkt.dst
+        temp_packet.src = pkt.src
+        # if len(self.packets) > 10:
+        #     del self.packets[0]
         if self.nextHop != self:   # 如果不是目的节点，向下一跳发送数据包
             self.flag = True
             if self.pair in route_table:
                 if route_table.index(self) < route_table.index(self.pair):  # 如果该校验节点在前
-                    self.send_packets(pkt, route_table)
-                    if self.count % 3 == 0:
+                    self.packets.append(temp_packet)  # 收到数据包
+                    self.send_packets(temp_packet, route_table)
+                    if self.count % 3 == 0:  # 每三个包校验一次
                         check_packet = check(pkt)
                         check_packet.src = self
                         check_packet.dst = self.pair
-                        #pkt.content = "falsified"  # 篡改数据包
                         self.packets.append(check_packet)
                         self.send_packets(check_packet, route_table)
                 elif route_table.index(self) > route_table.index(self.pair):  # 如果该校验节点在后
                     if pkt.dst != self:
-                        self.send_packets(pkt, route_table)
-                    if self.count % 3 == 0:
+                        self.send_packets(temp_packet, route_table)
+                    if self.count % 3 == 0:  # 每三个包校验一次
                         check_packet = check(pkt)
-                        self.packets.append(check_packet)
-                    if pkt.src == self.pair and pkt.dst == self:
+                        self.packets.append(check_packet)  # 将生成的校验包放入自己的缓存
+                    if temp_packet.src == self.pair and temp_packet.dst == self:
                         for item in self.packets:
-                            if item.content == pkt.content:
+                            if item.content == temp_packet.content:
+                                print(item.content)
                                 self.check_state = True
                         if self.check_state:
                             print(self.name + '处检测结果：数据包未被篡改')
@@ -79,4 +87,30 @@ class CheckNode(Node):
                             notify_packet.dst = route_table[len(route_table) - 1]
                             self.packets.append(notify_packet)
                             self.send_packets(notify_packet, route_table)
+                    self.packets.append(temp_packet)  # 收到数据包
                 self.count += 1
+                self.check_state = False
+            else:
+                self.send_packets(temp_packet, route_table)
+
+
+class FalsifyNode(Node):
+    def __init__(self, seq):
+        super(FalsifyNode, self).__init__(seq)
+        self.count = 0
+
+    def receive_packets(self, pkt, route_table):
+        temp_packet = Packet.Packet()
+        temp_packet.content = pkt.content
+        temp_packet.dst = pkt.dst
+        temp_packet.src = pkt.src
+        self.packets.append(temp_packet)
+        # if len(self.packets) > 5:
+        #     del self.packets[0]
+        if self.nextHop != self:  # 如果不是目的节点，向下一跳发送数据包
+            self.flag = True
+            if self.count % 3 == 0:  # 每三个包篡改一次
+                temp_packet.content = "falsified"
+                pass
+            self.count += 1
+            self.send_packets(temp_packet, route_table)
--- main.py
+++ main.py
@@ -5,23 +5,6 @@ import Topology, Node, Packet, time
 if __name__ == '__main__':
 
     node_list = []
-    '''name_list = ['a', 'b', 'c', 'f', 'g', 'h']
-
-    # 构造拓扑图
-    g = Graph.Graph()
-    g.add_vertex('a', {'b': 6, 'd': 2, 'f': 5})
-    g.add_vertex('b', {'a': 6, 'c': 4, 'd': 5})
-    g.add_vertex('c', {'b': 4, 'e': 4, 'h': 6})
-    g.add_vertex('d', {'a': 2, 'b': 5, 'e': 6, 'f': 4})
-    g.add_vertex('e', {'d': 6, 'c': 4, 'g': 5, 'h': 4})
-    g.add_vertex('f', {'a': 5, 'd': 4, 'g': 9})
-    g.add_vertex('g', {'f': 9, 'e': 5, 'h': 5})
-    g.add_vertex('h', {'c': 6, 'e': 4, 'g': 5})'''
-    start = 'a'
-    end = 'i'
-    shortestPath, length = Topology.topo.get_shortest_path(start, end)
-    shortestPath.reverse()
-    print('{}->{}的最短路径是：{}，最短路径为：{}'.format(start, end, shortestPath, length))
 
     # 实例化普通节点
     name_list = Topology.name_list1 + Topology.name_list2
@@ -30,7 +13,7 @@ if __name__ == '__main__':
         node_list.append(temp_node)
 
     # 实例化校验节点对
-# _________________________________________________________________________________
+    # _________________________________________________________________________________
     check_node_namelist = ['B', 'D', 'E', 'G']
     for i in range(2):
         for node in node_list:
@@ -44,41 +27,59 @@ if __name__ == '__main__':
     node_list[temp_len + 2].pair = node_list[temp_len]
     node_list[temp_len + 1].pair = node_list[temp_len + 3]
     node_list[temp_len + 3].pair = node_list[temp_len + 1]
-# _______________________________________________________________________________________
+    # _______________________________________________________________________________________
 
-    # 根据最短路，将相应的节点按次序放入路由表
-    route_table = []
-    for item in shortestPath:
-        for node in node_list:
-            if item == node.name:
-                route_table.append(node)
-
-    # 设置链路经过的节点的路由表
-    for node in route_table:
-        node.src = route_table[0]
-        node.dst = route_table[len(route_table) - 1]
-        if node != route_table[len(route_table) - 1]:
-            node.nextHop = route_table[route_table.index(node) + 1]
-
-    # 源节点发送数据包
-    content = ["Hello", "Simulator", "Hi", "my", "own", "network"]
+    # 实例化篡改节点
+    # _________________________________________________________________________________
+    falsify_node_list = ['C']
     for i in range(1):
-        route_table[0].packets = [Packet.Packet() for row in range(6)]
-        num = 0
-        for item in route_table[0].packets:
-            item.src = route_table[0]
-            item.dst = route_table[len(route_table) - 1]
-            item.content = content[num]
-            num += 1
-            route_table[0].send_packets(item, route_table)
+        for node in node_list:
+            if node.name in falsify_node_list:
+                node_list.remove(node)
+    for i in range(1):
+        falsify_node = Node.FalsifyNode(falsify_node_list[i])
+        node_list.append(falsify_node)
+    # _______________________________________________________________________________________
 
+    start = 'A'
+    end = ['I']
+    for j in range(len(end)):
+        shortestPath, length = Topology.topo.get_shortest_path(start, end[j])
+        shortestPath.reverse()
+        print('{}->{}的最短路径是：{}，最短路径为：{}'.format(start, end[j], shortestPath, length))
+
+        # 根据最短路，将相应的节点按次序放入路由表
+        route_table = []
+        for item in shortestPath:
+            for node in node_list:
+                if item == node.name:
+                    route_table.append(node)
+
+        # 设置链路经过的节点的路由表
         for node in route_table:
-            print(node.name + '\t', end='')
-            for pkt in node.packets:
-                #print(pkt.src.name + '\t' + pkt.dst.name + '\t' + pkt.content + '\t', end='')
-                print(pkt.content + '\t', end='')
-            print('')
+            node.src = route_table[0]
+            node.dst = route_table[len(route_table) - 1]
+            if node != route_table[len(route_table) - 1]:
+                node.nextHop = route_table[route_table.index(node) + 1]
 
-        time.sleep(0.1)
-        print('____________________________')
-        print(len(node_list))
+        # 源节点发送数据包
+        content = ["Hello", "Simulator", "Hi", "my", "own", "network", "Wonderful"]
+        for i in range(1):
+            route_table[0].packets = [Packet.Packet() for row in range(1)]
+            num = 0
+            for item in route_table[0].packets:
+                item.src = route_table[0]
+                item.dst = route_table[len(route_table) - 1]
+                item.content = content[num]
+                num += 1
+                route_table[0].send_packets(item, route_table)
+
+            for node in route_table:
+                print(node.name + '\t', end='')
+                for pkt in node.packets:
+                    #print(pkt.src.name + '\t' + pkt.dst.name + '\t' + pkt.content + '\t', end='')
+                    print('|' + pkt.content + '|' +  '\t', end='')
+                print('')
+
+            time.sleep(0.1)
+            print('____________________________________________________________________________')
