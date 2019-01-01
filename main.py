61882bac122c5c20f0f0b2f2efe8d6c2609d28f2
cx-z <1612379420@qq.com>
2019-01-01 09:15:01 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import threading
 import time
 
 
-def simulator_start(src_node, dst_node):
+def simulator_start(src_node, dst_node, content):
     start = src_node.address
     end = dst_node.address
     shortestPath, length = Topology.topo.get_shortest_path(start, end)
@@ -14,7 +14,6 @@ def simulator_start(src_node, dst_node):
     print('{}->{}的最短路径是：{}，最短路径为：{}'.format(start, end, shortestPath, length))
 
     # 源节点发送数据包
-    content = ["Hello", "Simulator", "Hi", "my", "own", "network", "Wonderful"]
     src_node.packets = [Packet.Packet() for row in range(3)]
     num = 0
     print('包个数' + str(len(src_node.packets)))
@@ -24,8 +23,7 @@ def simulator_start(src_node, dst_node):
         item.content = content[num]
         num += 1
         item.seq_num = num
-        src_node.send_packets(item, node_list)
-        print("源节点发包" + item.content)
+        src_node.entry.append(item)
 
 
 if __name__ == '__main__':
@@ -37,16 +35,17 @@ if __name__ == '__main__':
         temp_node = Node.Node(item)
         node_list.append(temp_node)
 
+    content1 = ["Hello", "Simulator", "Hi", "my", "own", "network", "Wonderful"]
+    content2 = ["I", "am", "C"]
+    simulator_start(node_list[0], node_list[-1], content1)
+    simulator_start(node_list[2], node_list[-1], content2)
+
     threads = []
     for i in range(len(node_list)):
-        thread = threading.Thread(target=node_list[i].receive_packets,args=(node_list,))
+        thread = threading.Thread(target=node_list[i].send_packets, args=(node_list,))
         threads.append(thread)
-    thread = threading.Thread(target=simulator_start, args=(node_list[0], node_list[-1]))
-    start_time = time.time()
     for t in threads:
         t.start()
-    thread.start()
-    thread.join(2)
     for t in threads:
         t.join(2)
 
@@ -55,4 +54,4 @@ if __name__ == '__main__':
         for pkt in node.packets:
             print('| {} : {} |\t'.format(pkt.seq_num, pkt.content), end='')
         print('')
-    print('_______________________________________________________________________')
+    print('_______________________________END________________________________________')
